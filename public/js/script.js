0aab2cddff3b3a10d639b1df8df8387c409426c8
Mihir More <mihirmore.25@gmail.com>
2025-01-31 07:31:01 +0530

    Modify from inline css to external css for custom animation in public for create and edit recipe

--- public/js/script.js
+++ public/js/script.js
@@ -12,3 +12,26 @@ setTimeout(() => {
     errorMessage.style.display = "none";
     errorMessage.remove();
 }, 3000);
+
+let addIngredientsBtn = document.getElementById("addIngredientsBtn");
+let addInstructionsBtn = document.getElementById("addInstructionsBtn");
+let instructionList = document.querySelector(".instructionList");
+let ingredientList = document.querySelector(".ingredientList");
+let ingredientDiv = document.querySelectorAll(".ingredientDiv")[0];
+let instructionDiv = document.querySelectorAll(".instructionDiv")[0];
+
+addIngredientsBtn.addEventListener("click", function () {
+    let newIngredients = ingredientDiv.cloneNode(true);
+    let input = newIngredients.getElementsByTagName("input")[0];
+    input.value = "";
+    ingredientList.appendChild(newIngredients);
+    console.log("Ingredients Button Clicked");
+});
+
+addInstructionsBtn.addEventListener("click", function () {
+    let newInstructions = instructionDiv.cloneNode(true);
+    let input = newInstructions.getElementsByTagName("input")[0];
+    input.value = "";
+    instructionList.appendChild(newInstructions);
+    console.log("Instructions Button Clicked");
+});
