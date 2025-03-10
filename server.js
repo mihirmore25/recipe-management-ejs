b7874fc0464203799b69c36a47c611e7f8ad8cdf
Mihir More <mihirmore.25@gmail.com>
2025-03-10 18:00:17 +0530

    Update helmet middleware content security policy for img-src

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(
         contentSecurityPolicy: {
             directives: {
                 ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-                "img-src": ["'self'", "data:", "http://res.cloudinary.com/"],
+                "img-src": ["'self'", "data:", "blob:", "http://res.cloudinary.com/"],
             },
         },
     })
