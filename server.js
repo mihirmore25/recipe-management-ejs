4317908f9f66caf5f8d9ae327cabce06d1cba129
Mihir More <mihirmore.25@gmail.com>
2025-07-01 07:59:23 +0530

    Remove cronjob from server.js already exist as middleware

--- server.js
+++ server.js
@@ -74,22 +74,22 @@ app.use(morgan("dev"));
 app.set("view engine", "ejs");
 // app.use(express.static(__dirname + "/public"));
 
-// Cron job to delete guest users every 15 minutes
-cron.schedule("*/15 * * * *", async () => {
-    try {
-        const result = await User.deleteMany({
-            role: "guest", // Only delete users with the "guest" role
-            createdAt: { $lt: new Date(Date.now() - 2 * 60 * 1000) }, // Older than 2 minutes
-        });
-        console.log(
-            `${
-                result.deletedCount
-            } guest users deleted at ${new Date().toISOString()}`
-        );
-    } catch (error) {
-        console.error("Error deleting guest users:", error);
-    }
-});
+// Cron job to delete guest users every 30 minutes
+// cron.schedule("0,30 * * * *", async () => {
+//     try {
+//         const result = await User.deleteMany({
+//             role: "guest", // Only delete users with the "guest" role
+//             createdAt: { $lt: new Date(Date.now() - 2 * 60 * 1000) }, // Older than 2 minutes
+//         });
+//         console.log(
+//             `${
+//                 result.deletedCount
+//             } guest users deleted at ${new Date().toISOString()}`
+//         );
+//     } catch (error) {
+//         console.error("Error deleting guest users:", error);
+//     }
+// });
 
 // Routes
 app.use("/", authRoutes);
